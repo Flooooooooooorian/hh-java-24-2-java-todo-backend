60cae369464797ef5a1143ef64d60505f66749b7
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2024-06-18 10:12:45 +0200

    Add Security

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import {allPossibleTodos} from "./TodoStatus.ts";
 
 function App() {
 
+    const [user, setUser] = useState<string | null | undefined>(undefined)
     const [todos, setTodos] = useState<Todo[]>()
 
     function fetchTodos() {
@@ -15,29 +16,57 @@ function App() {
             })
     }
 
-    useEffect(fetchTodos, [])
+    useEffect(() => {
+        loadUser()
+            .then(() => {
+                fetchTodos()
+            })
+    }, [])
 
-    if (!todos) {
-        return "Lade..."
+    function login() {
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
+
+        window.open(host + '/oauth2/authorization/github', '_self')
+    }
+
+    function logout() {
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
+
+        window.open(host + '/logout', '_self')
+    }
+
+    const loadUser = () => {
+        return axios.get('/api/auth/me')
+            .then(response => {
+                setUser(response.data)
+            })
+            .catch(error => {
+                setUser(null)
+                throw error
+            })
     }
 
     return (
-        <>
+        <div>
+            <h1>TODOs</h1>
+            {!user && <button onClick={login}>Login</button>}
+            {user && <button onClick={logout}>Logout</button>}
             <div className="page">
-                <h1>TODOs</h1>
                 {
-                    allPossibleTodos.map(status => {
-                        const filteredTodos = todos.filter(todo => todo.status === status)
-                        return <TodoColumn
-                            status={status}
-                            todos={filteredTodos}
-                            onTodoItemChange={fetchTodos}
-                            key={status}
-                        />
-                    })
+                    todos
+                        ? allPossibleTodos.map(status => {
+                            const filteredTodos = todos.filter(todo => todo.status === status)
+                            return <TodoColumn
+                                status={status}
+                                todos={filteredTodos}
+                                onTodoItemChange={fetchTodos}
+                                key={status}
+                            />
+                        })
+                        : <p>Loading...</p>
                 }
             </div>
-        </>
+        </div>
     )
 }
 
